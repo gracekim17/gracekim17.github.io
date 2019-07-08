5a21db944418bd8a3fa055ea18dea34d42b1c526
Jeong Hwan Kim <jhkim@microsoft.com>
2019-07-07 22:39:25 -0700

    updated webserver.js

--- webserver.js
+++ webserver.js
@@ -1,9 +1,28 @@
 var http = require('http');
 
 function doDoHttpServer(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.writeHead(200, {'foo': 'bar'});
-    res.end('Hello. My name is Grace Kim.');
+    if (req.url == "/") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('You requested /. Here is the default document content');
+    } else if (req.url == "/foo") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(`<html>
+<body>
+<h1>You requested /foo.</h1>
+Here is the response for it
+</body>
+</html>`
+        );
+    } else {
+        res.writeHead(404, {'foo': 'bar'});
+        res.end(`<html>
+<body>
+<h1>You requested ` +  req.url + `.</h1>
+The URL is not found in this web server.
+</body>
+</html>`
+        );
+    }
 }
 
 var server = http.createServer(doDoHttpServer);

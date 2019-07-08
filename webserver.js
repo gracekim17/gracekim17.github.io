b5c90ff44bab9e3485b6b3be5322b716749438fd
Jeong Hwan Kim <jhkim@microsoft.com>
2019-07-07 23:59:51 -0700

    translate

--- webserver.js
+++ webserver.js
@@ -1,15 +1,37 @@
-var http = require('http');
+﻿var http = require('http');
+
+var data = {
+    "foo":"bar",
+    "foo2":"bar2"
+}
 
 function doDoHttpServer(req, res) {
     if (req.url == "/") {
         res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': 'null'});
         res.end('You requested /. Here is the default document content');
+    } else if (req.url == "/game/read") {
+        res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': 'null'});
+        res.end(JSON.stringify(data));
+    } else if (req.url == "/game/write") {
+        res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': 'null'});
+        data[req.headers.myname] = req.headers.myvalue;
+        res.end("write was successful!!!");
+    } else if (req.url.startsWith("/game/translate?")) {
+        res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': 'null'});
+        var queryString = req.url.replace("/game/translate?", "");
+        queryString = queryString.replace("%20", " ");
+        if (queryString == "Grace Kim") {
+            res.end('<html><head><meta charset="utf-8"></head><body>김예지</body></html>');
+        }
+        if (queryString == "JeongHwan Kim") {
+            res.end('<html><head><meta charset="utf-8"></head><body>김정환</body></html>');
+        }
     } else if (req.url == "/test.htm") {
         res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': 'null'});
         res.end(`{
             "me" : {
                 "lastName":"Kim",
-                        "firstName":"Grace"
+                "firstName":"Grace"
             }
         }`
         );
